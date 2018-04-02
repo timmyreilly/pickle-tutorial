04a718157182c1a0d8e0da68541199ef04a8c704
Tim Reilly <tireilly@microsoft.com>
2018-04-02 15:43:32 -0700

    adding to pickle

--- pickle-web3.py
+++ pickle-web3.py
@@ -73,12 +73,67 @@ print("This is how you print text to the screen")
 
 print (" nnnn \ nnnn")
 
-for (x in " nnnn \ nnnn"
+try:
+    for x in 3: 
+        print (" x ") 
+except TypeError: 
+    print ('woops') 
+
+# Welp first of all that's an integer. 
+# Integers are numbers: 3 is an integer. 
+
+try: 
+    for x in "":
+        print (" x ")
+except TypeError: 
+    print ('woops') 
+
+try: 
+    for x in "xxx":
+        print ( " x "); 
+except TypeError: 
+    print ('woops') # that's three woops yall 
+
+
+# wait a second there are only 3 one woops... What happened back there?
+
+print ( 'Part II') 
+
+
+for x in "xxx":
     print (" x ") 
 
 
+for x in "xxxvvv":
+    print (" xx ")
+
+for x in x: 
+    print (" xxx ")
+
+x = x 
+
+x == x 
+
+print ( x ) 
+
+print (x ) 
+
+x == x; 
+
+try: 
+    print ( x.value ) 
+except:
+    print (" woops ")
+
+
+
+
+
+
 ''' ON a side note what the heck does tab+m do on visual studio code. Sorry I mean to say
-what is the 
+what is the ctrl+m turns on tab for focus and I have no idea why you would use it. '''
+
+
 
 
 
